6086dad69aeb51416126ce7a35c4a196e02d04cc
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-02-09 09:26:53 +0100

    Add connect wallet

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,15 +1,28 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import site from "config/site";
 import Head from "next/head";
 import { PropsWithChildren } from "react";
 
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between p-2">
+      <div className="text-xs font-bold uppercase tracking-wider">
+        QF Relief
+      </div>
+      <ConnectButton />
+    </header>
+  );
+};
 export const Layout = (props: PropsWithChildren) => {
   return (
     <>
       <Head>
-        <title>qf-relief</title>
-        <meta name="description" content="" />
+        <title>{site.title}</title>
+        <meta name="description" content={site.description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="prose-xl flex min-h-screen flex-col bg-stone-50 text-stone-800">
+      <main className="flex min-h-screen flex-col bg-stone-50 text-stone-800">
+        <Header />
         <div className="flex-1">{props.children}</div>
         <footer className=" bg-stone-900 p-32"></footer>
       </main>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,34 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+
+import { mainnet, goerli } from "wagmi/chains";
 
 import "../styles/globals.css";
 
+import site from "config/site";
+
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [mainnet, goerli],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({ appName: site.title, chains });
+
+const wagmiClient = createClient({ autoConnect: true, connectors, provider });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 };
 
 export default MyApp;
