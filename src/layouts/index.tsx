fd9b9d07eeade454c4dc9a41a65b7f1a8f25c473
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-02-09 15:58:16 +0100

    Remove header from layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,19 +1,7 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Link } from "components/Link";
 import site from "config/site";
 import Head from "next/head";
 import { PropsWithChildren } from "react";
 
-const Header = () => {
-  return (
-    <header className="flex items-center justify-between p-2">
-      <div className="flex items-center gap-8">
-        <div className="text-xs font-bold  tracking-wider">QF relief</div>
-      </div>
-      <ConnectButton />
-    </header>
-  );
-};
 export const Layout = (props: PropsWithChildren) => {
   return (
     <>
@@ -23,7 +11,6 @@ export const Layout = (props: PropsWithChildren) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-stone-50 text-stone-800">
-        <Header />
         <div className="pb-24">{props.children}</div>
         <footer className=" py-24"></footer>
       </main>
